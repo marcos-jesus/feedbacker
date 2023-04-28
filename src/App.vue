f2bab4b1b3236812bf635eef48f20f10b7a408c0
Marcos Jesus <marcosjesus.dev@gmail.com>
2023-04-28 00:26:15 -0300

    User Loged

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,25 @@
   <router-view />
 </template>
 <script>
+import { watch } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 import ModalFactory from '@/components/ModalFactory/IndexModal'
 export default {
-  components: { ModalFactory }
+  components: { ModalFactory },
+
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    watch(() => route.path, () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token')
+
+        if (!token) {
+          router.push({ name: 'Home' })
+        }
+      }
+    })
+  }
 }
 </script>
